5958c29eaf12d8dd3aadc11004789c20b3142aa5
Alexander Coleman <alexanderjcoleman01@gmail.com>
2022-07-06 11:35:44 -0500

    first commit

---
+++ OperatorExercise/Methods.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorExercise
+{
+    public class Methods
+    {
+
+        public static void AreaOfCircle()
+        {
+            Console.WriteLine("What is the radius of your circle");
+
+            double radius = Convert.ToDouble(Console.ReadLine());
+            double area = Math.Round(Math.PI * Math.Pow(radius, 2),2);
+
+            Console.WriteLine($"The area of a circle with a radius of {radius} is: {area}");
+        }
+    }
+}
---
+++ OperatorExercise/Program.cs
@@ -0,0 +1,26 @@
+﻿using OperatorExercise;
+
+int a = 17;
+int b = 4;
+int quotient;
+int remainder;
+
+int sum = a + b;
+Console.WriteLine(sum);
+
+int difference = a - b;
+Console.WriteLine(difference);
+
+int product = a * b;
+Console.WriteLine(product);
+
+quotient = 17 / 4;
+remainder = a % b;
+
+string result = $"{a}/{b} is {quotient} remainder {remainder}";
+Console.WriteLine(result);
+
+
+Methods.AreaOfCircle(); 
+
+
